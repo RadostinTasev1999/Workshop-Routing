ccecf8798155b46903d089af0ed6e8820714e82f
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-09-21 13:54:38 +0300

    Define Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css'
+import {Routes, Route} from 'react-router'
+// Components
 import Home from './components/home/Home'
 import Header from './components/Header/header'
-import {Routes, Route} from 'react-router'
+import Catalog from './components/catalog/Catalog'
+import Create from './components/create/Create'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
 
 function App() {
  
@@ -13,8 +18,11 @@ function App() {
     <main id="main-content">
       <Routes>
         <Route index element={<Home />}/>
+        <Route path='/games' element={<Catalog/>}/>
+        <Route path='/games/create' element={<Create />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
-      <Home />
     </main>
 
       </div>
