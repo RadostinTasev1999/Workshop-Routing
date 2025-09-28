ca0038270f4c30768a4e2f52099a01fd05c632e5
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-09-28 21:29:37 +0300

    Update gameService methods

--- client/src/api/gameService.js
+++ client/src/api/gameService.js
@@ -1,15 +1,38 @@
-import { request } from "../utils/requester";
+import request from '../utils/requester.js'
 
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
 
 export default {
      create(gameData){
-       const createGame = request('POST',baseUrl,gameData)
-
-       return createGame
+    //    const createGame = request('POST',baseUrl,gameData)
+        const createGame = request.post(baseUrl,gameData)
+       
+        return createGame
     },
     async getAll(){
-        return request('GET',baseUrl)
+        const result = await request.get(baseUrl)
+
+        const games = Object.values(result)
+
+         console.log('Result from getAll method is:', games)
+
+        return games
+    },
+
+    async gameDetails(_id){
+        
+        const result = await request.get(`${baseUrl}/${_id}`)
+
+        console.log('Game details are:', result)
+        return result
+
+    },
+
+    async editGame(payload,_id){
+        
+        const result = await request.put(`${baseUrl}/${_id}`, payload)
+
+        return result
     }
 }
