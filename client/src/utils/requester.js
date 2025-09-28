cda095b9a1d18406aef89465a7c9ab18310a1e27
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-09-28 21:26:31 +0300

    Update requester.js

--- client/src/utils/requester.js
+++ client/src/utils/requester.js
@@ -20,7 +20,16 @@ export const request = async (method,url,data) => {
 
      
     const response = await fetch(url,options)
+    console.log('Response in requester is:', response)
     const result = await response.json()
 
     return result;
+}
+
+export default {
+    get: request.bind(null,'GET'),
+    //get:(params) => request('GET', ...params),
+    post: request.bind(null,'POST'),
+    put: request.bind(null,'PUT'),
+    delete: request.bind(null,'GET')
 }
