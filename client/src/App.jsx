36f121a06ca46bd5e7803254bb94d1584be5ecb8
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-09-28 21:29:16 +0300

    Add routes for GameDetails and EditGame

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import {Routes, Route} from 'react-router'
 // Components
 import Home from './components/home/Home'
-import Header from './components/Header/header'
+import Header from './components/header/Header'
 import Catalog from './components/catalog/Catalog'
 import Create from './components/create/Create'
 import Login from './components/login/Login'
 import Register from './components/register/Register'
+import GameDetails from './components/details/GameDetails'
+import EditGame from './components/edit/EditGame'
 
 function App() {
  
@@ -19,6 +21,8 @@ function App() {
       <Routes>
         <Route index element={<Home />}/>
         <Route path='/games' element={<Catalog/>}/>
+        <Route path='/games/:gameId' element={<GameDetails/>}/>
+        <Route path='/games/:gameId/edit' element={<EditGame/>}/>
         <Route path='/games/create' element={<Create />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
