1893d92c010c8e68fb4fb358b32c94bc912ae831
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-10-12 16:39:52 +0300

    create authApi.js

---
+++ client/src/api/authApi.js
@@ -0,0 +1,58 @@
+import { useEffect } from 'react';
+import request from '../utils/requester'
+import { useRef } from 'react';
+
+const baseUrl = 'http://localhost:3030/users'
+
+export const useLogin = () => {
+
+   const abortRef = useRef(new AbortController())
+   // call useRef() to declare a ref
+    
+     const login = async (email, password) => {
+        return request.post(`${baseUrl}/login`, {email, password}, { signal: abortRef.current.signal })
+     }
+
+     useEffect(() => {     
+
+      const abortController = abortRef.current;
+      
+       return () => abortController.abort()
+       /*
+       cleanup function will be called once, just before
+       the component that uses useLogin is removed from the DOM(unmounted)
+       */
+     },[]);
+
+     /*
+     useEffect is tied to the lifecycle of the component that uses the hook.
+     */
+
+     return {
+        login
+     }
+
+}
+
+export const useRegister = () => {
+
+   const register = (email,password) => {
+
+      const payload = {
+         email,
+         password
+      }
+         return request.post(`${baseUrl}/register`, payload)
+   }
+
+   return {
+      register
+   }
+
+}
+
+/*
+2 types of hooks:
+onMount
+onEvent
+*/
