6ce55bb749b59f1ddf310a4e532cc685a46b8172
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-10-25 15:21:45 +0300

    update App.jsx
    update imports
    render UserProvider
    declare AuthGuard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,32 +9,34 @@ import Login from './components/login/Login'
 import Register from './components/register/Register'
 import GameDetails from './components/details/GameDetails'
 import EditGame from './components/edit/EditGame'
-import UserProvider from './providers/UserProvider'
 import Logout from './components/logout/Logout'
-// import { useContext } from 'react'
-import { UserContext } from './contexts/UserContext'
+import { UserProvider } from './providers/UserProvider'
+import AuthGuard from './components/guards/authGuard'
 
 function App() {
 
   return (
+    
     <>
-      <div id="box"> 
-    <UserProvider>
-      <Header />
+    <UserProvider >
+    <div id="box"> 
+        <Header />
     <main id="main-content">
       <Routes>
         <Route index element={<Home />}/>
         <Route path='/games' element={<Catalog/>}/>
         <Route path='/games/:gameId' element={<GameDetails/>}/>
-        <Route path='/games/:gameId/edit' element={<EditGame/>}/>
-        <Route path='/games/create' element={<Create />} />
+        <Route element={<AuthGuard />}>
+          <Route path='/games/create' element={<Create />} />
+          <Route path='/games/:gameId/edit' element={<EditGame/>}/>
+          <Route path='/logout' element={<Logout />} />
+        </Route>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/logout' element={<Logout />} />
       </Routes>
-    </main>
-    </UserProvider>
+    </main> 
       </div>
+    </UserProvider>
     </>
   )
 }
