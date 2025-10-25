1ec8c539ff0f23d6653eadcec961582623423b12
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-10-25 15:22:04 +0300

    Create authGuard component

---
+++ client/src/components/guards/authGuard.jsx
@@ -0,0 +1,14 @@
+import useAuth from "../../hooks/useAuth";
+import { Navigate, Outlet } from "react-router";
+
+export default function AuthGuard(){
+    const { isAuthenticated } = useAuth();
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" />
+    }
+
+    return(
+        <Outlet />
+    );
+}   
