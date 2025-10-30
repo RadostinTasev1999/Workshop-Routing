33f3b22ec3bf12f0afcf3d00a1fe35c0bebcc13a
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-10-30 08:07:22 +0200

    update useAuth.js
    use useCallback hook to memoize value of requestWrapper
    
    use useMemo hook to cache value of requestObject

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 import { useUserContext } from "./useUserContext";
 import { UserContext } from "../contexts/UserContext"
 import request from "../utils/requester"
+import { useCallback, useMemo } from "react";
 
 export default function useAuth() {
 
@@ -12,29 +13,39 @@ export default function useAuth() {
         }
     }
 
-    const requestWrapper = (method,url, data, options = {}) => {
+    const requestWrapper = useCallback((method,url, data, options = {}) => {
 
-        const optionWrapper = {
+        const authOptions = {
             ...options,
-         headers:{
-            'X-Authorization': authData.accessToken,
-            ...options.headers
+              headers:{
+                 'X-Authorization': authData.accessToken,
+                 ...options.headers
         }
-            }
-            
-       return request.baseRequest(method,url,data,authData.accessToken ? optionWrapper : options)
-    } 
+    }
 
-    return {
-        ...authData,
-        isAuthenticated: !!authData.accessToken,
-        options,
-        request: {
+    return request.baseRequest(method,url,data,authData.accessToken ? authOptions : options)
+
+},[authData.accessToken])
+    
+     
+// TODO: use UseMemo (https://react.dev/reference/react/useMemo)
+const requestObject = useMemo(() => (
+    // outer function
+    {
             get: requestWrapper.bind(null,'GET'),
             put: requestWrapper.bind(null,'PUT'),
             post: requestWrapper.bind(null,'POST'),
             delete: requestWrapper.bind(null,'DELETE')
-        }
+    
+}
+    ),[requestWrapper])
+
+    return {
+        ...authData,
+        isAuthenticated: !!authData.accessToken,
+        userId: authData._id,
+        options,
+        request: requestObject
     }
 
 }
