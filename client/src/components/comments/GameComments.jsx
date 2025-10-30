3ed68ac2b7cc2b46e36e7566b3bc85bd9d40880d
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-10-30 08:04:08 +0200

    Update GameComments component

--- client/src/components/comments/GameComments.jsx
+++ client/src/components/comments/GameComments.jsx
@@ -1,33 +1,16 @@
-import { useNavigate } from "react-router"
-import { useCreateComment } from "../../api/commentApi"
-
-
-
-export default function GameComments(
-    { gameId }
-){
-
-    const navigate = useNavigate()
-    const { createComment } = useCreateComment()
-
-    const onCreateComment = async(formData) => {
-
-        const { comment } = Object.fromEntries(formData)
-
-        await createComment(comment,gameId)
- 
-        navigate(`/games/${gameId}`)
-        
-    }
-
-
+export default function GameComments(){
     return (
-        <article className="create-comment">
-            <label>Add new comment:</label>
-            <form className="form" action={onCreateComment}>
-                <textarea name="comment" placeholder="Comment......"></textarea>
-                <input className="btn submit" type="submit" value="Add Comment" />
-            </form>
-        </article>
+        <div className="details-comments">
+                    <h2>Comments:</h2>
+                    <ul>
+                        <li className="comment">
+                            <p>Content: I rate this one quite highly.</p>
+                        </li>
+                        <li className="comment">
+                            <p>Content: The best game.</p>
+                        </li>
+                    </ul>
+                    <p className="no-comment">No comments.</p>
+                </div>
     )
 }
