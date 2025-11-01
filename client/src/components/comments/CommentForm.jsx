3c21614bf2ec406be2d4c1fd6d1fbc440624fe70
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-01 13:41:51 +0200

    update CommentForm
    bind submit handler function to the form

--- client/src/components/comments/CommentForm.jsx
+++ client/src/components/comments/CommentForm.jsx
@@ -1,30 +1,11 @@
-import { useNavigate } from "react-router"
-import { useCreateComment } from "../../api/commentApi"
-
-
-
 export default function CommentForm(
-    { gameId }
+    { formAction }
 ){
-
-    const navigate = useNavigate()
-    const { createComment } = useCreateComment()
-
-    const onCreateComment = async(formData) => {
-
-        const { comment } = Object.fromEntries(formData)
-
-        await createComment(comment,gameId)
- 
-        navigate(`/games/${gameId}`)
-        
-    }
-
 
     return (
         <article className="create-comment">
             <label>Add new comment:</label>
-            <form className="form" action={onCreateComment}>
+            <form className="form" action={formAction}>
                 <textarea name="comment" placeholder="Comment......"></textarea>
                 <input className="btn submit" type="submit" value="Add Comment" />
             </form>
