43179c310eb999ea0f3bd02b7d7421ef0c637c7d
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-02 09:37:12 +0200

    Update commentApi.js
    add relations to the search parameters
    expose addComment prop
    expose gameComments state
    add author prop to payload in createComment method

--- client/src/api/commentApi.js
+++ client/src/api/commentApi.js
@@ -1,17 +1,21 @@
 import { useEffect, useReducer } from "react"
 import useAuth from "../hooks/useAuth"
 // import request from "../utils/requester"
-import { useState } from "react"
+
 import request from '../utils/requester'
 
 const baseUrl = 'http://localhost:3030/data/comments'
 
 function commentsReducer(state, action){
+    // action = {type: 'GET_ALL', payload: response}
+    // action = { type: 'ADD_COMMENT', payload: commentData }
+    console.log('Action in commentsReducer is:', action)
 
     switch (action.type) {
         case 'GET_ALL':
             return action.payload
-        
+        case 'ADD_COMMENT':
+            return [...state, action.payload]
         default:
             return state
     }
@@ -23,13 +27,14 @@ export const useComments = (gameId) => {
     //const { request } = useAuth()
     // const [gameComments, setComments] = useState([])
 
-    const [comments, dispatch] = useReducer(commentsReducer,[]) // reducer is a function which knows how to update the state
-
+    const [gameComments, dispatch] = useReducer(commentsReducer,[]) // reducer is a function which knows how to update the state
+    console.log('Game comments are:', gameComments)
     const { accessToken } = useAuth()
 
     useEffect(() => {
         const searchParams = new URLSearchParams({
-            where: `gameId="${gameId}"`
+            where: `gameId="${gameId}"`,
+            load: `author=_ownerId:users`
         })
 
         const options = {
@@ -37,27 +42,29 @@ export const useComments = (gameId) => {
                 'X-Authorization': accessToken
             }
         }
-        
+        console.log('URL endpoint is:', `${baseUrl}?${searchParams.toString()}`)
         request.get(`${baseUrl}?${searchParams.toString()}`,null,options)
             .then((response) => dispatch({type: 'GET_ALL', payload: response}))
     },[gameId, accessToken])
     
     return {
-        comments,
-        addComment: dispatch
+        gameComments,
+        addComment: (commentData) => dispatch({ type: 'ADD_COMMENT', payload: commentData })
     }
 }
 
 export const useCreateComment = () => {
 
-    const { request } = useAuth()
-    // const { userId } = useAuth()
+    const { request,email } = useAuth()
 
     const createComment = (comment, gameId) => {
 
         const payload = {
             comment,
-            gameId
+            gameId,
+            author:{
+                email
+            }
         }
 
         return request.post(baseUrl,payload)
