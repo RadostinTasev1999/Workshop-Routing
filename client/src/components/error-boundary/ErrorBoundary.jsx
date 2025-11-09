090322a0d9fb64a4c0a4be387c933f0e36650d27
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-11-09 08:58:45 +0200

    Create ErrorBoundary
    declare constructor
    initialize state
    declare static method getDerivedStateFromError()
    componentDidCatch()

---
+++ client/src/components/error-boundary/ErrorBoundary.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+
+    constructor(props){
+        console.log('Initialize error boundary')
+        super(props)
+
+        // Initialize state
+        this.state = {
+            hasError: false,
+            errorMessage: ''
+        }
+    }
+    
+    static getDerivedStateFromError(error){
+        console.log('getDerivedStateFromError')
+
+        return {
+            hasError: true,
+            errorMessage: error.message
+        }
+
+    }
+
+    componentDidCatch(error,errorInfo){
+        console.log('Component did catch')
+    }
+
+    
+
+    render(){
+        
+        if (this.state.hasError) {
+            return (
+                <>
+                    <h1>Error Page</h1>
+                    <p>{this.state.errorMessage}</p>
+                </>
+            );
+    }      
+
+        return this.props.children;
+    }
+}
